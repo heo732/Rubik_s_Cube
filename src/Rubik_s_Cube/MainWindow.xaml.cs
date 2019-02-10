c668f82246862048916fc79a7b46408cad1c757b
heo732 <andriy.buzhak.1@gmail.com>
2019-02-10 14:13:19 +0300

    v0.0.1    Change: MainWindow.xaml, MainWindow.xaml.cs

--- src/Rubik_s_Cube/MainWindow.xaml.cs
+++ src/Rubik_s_Cube/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SharpGL.SceneGraph;
+using SharpGL;
+using SharpGL.Shaders;
 
 namespace Rubik_s_Cube
 {
@@ -20,9 +23,74 @@ namespace Rubik_s_Cube
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Scene scene = new Scene();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the OpenGLDraw event of the openGLControl1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="args">The <see cref="SharpGL.SceneGraph.OpenGLEventArgs"/> instance containing the event data.</param>
+        private void openGLControl_OpenGLDraw(object sender, OpenGLEventArgs args)
+        {
+            //  Draw the scene.
+            scene.Draw(openGLControl.OpenGL);
+        }
+
+        /// <summary>
+        /// Handles the OpenGLInitialized event of the openGLControl1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="args">The <see cref="SharpGL.SceneGraph.OpenGLEventArgs"/> instance containing the event data.</param>
+        private void openGLControl_OpenGLInitialized(object sender, OpenGLEventArgs args)
+        {
+            //  Initialise the scene.
+            scene.Initialise(openGLControl.OpenGL, (float)Width, (float)Height);
+        }
+
+        /// <summary>
+        /// Handles the Resized event of the openGLControl1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="args">The <see cref="SharpGL.SceneGraph.OpenGLEventArgs"/> instance containing the event data.</param>
+        private void openGLControl_Resized(object sender, OpenGLEventArgs args)
+        {
+            //  TODO: Set the projection matrix here.
+
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+            //  Load the identity.
+            gl.LoadIdentity();
+
+            //  Create a perspective transformation.
+            gl.Perspective(60.0f, (double)Width / (double)Height, 0.01, 100.0);
+
+            //  Use the 'look at' helper function to position and aim the camera.
+            gl.LookAt(-5, 5, -5, 0, 0, 0, 0, 1, 0);
+
+            //  Set the modelview matrix.
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        public void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                containerMenu.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+
+        public void buttonUp_Click(object sender, RoutedEventArgs e)
+        {
+            containerMenu.Visibility = System.Windows.Visibility.Hidden;
         }
     }
 }
